61df404b465eac1da71cc589e87ac5747482f2b6
Bailey <baileygardner25@gmail.com>
2019-11-10 20:43:10 +0000

    fix

--- AdvancedGrapGames/nclgl/OGLRenderer.h
+++ AdvancedGrapGames/nclgl/OGLRenderer.h
@@ -30,7 +30,7 @@ _-_-_-_-_-_-_-""  ""
 #include "Quaternion.h"
 #include "Matrix4.h"
 #include "Window.h"
-#include "light.h"
+#include "Light.h"
 
 #include "Shader.h"		//Students make this file...
 #include "Mesh.h"		//And this one...
